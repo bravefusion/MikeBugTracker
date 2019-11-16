bbb1e3f3174dc18fc36881c7d745de8c4562dc9e
bravefusion <50847488+bravefusion@users.noreply.github.com>
2019-11-16 03:04:07 -0500

    Change

--- MikeBugTracker/Controllers/HomeController.cs
+++ MikeBugTracker/Controllers/HomeController.cs
@@ -1,15 +1,35 @@
-﻿using System;
+﻿using MikeBugTracker.Helpers;
+using MikeBugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace MikeBugTracker.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private UserRolesHelper rolesHelper = new UserRolesHelper();
         public ActionResult Index()
         {
+            if (User.IsInRole("Admin,Demo_Admin"))
+            {
+                var aData = new IndexViewModel();
+                aData.allTickets = db.Tickets.ToList();
+                aData.myProjects = db.Projects.ToList();
+                aData.Users = db.Users.ToList();
+                return View(aData);
+            }
+            else if (User.IsInRole("Project Manager,Demo_Project Manager"))
+            {
+                var userId =  User.Identity.GetUserId();
+                var myData = new IndexViewModel();
+                myData.myProjects = db.Projects.Where(p => p.Name == userId ).ToList();
+                return View(myData);
+            }
             return View();
         }
 
